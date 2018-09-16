7b6dee4f84ce449b381fa742d188b0d7e71a71fa
John Williams <jww335@gmail.com>
2018-09-16 19:21:13 -0400

    added tweet modal methods

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,11 @@ class WelcomeController < ApplicationController
   # GET /welcomes
   # GET /welcomes.json
   def index
-
+    # @month = Tweet.tweets_this_month
+    # puts @month
+    @tweets = Tweet.first
+    @tweets.tweets_this_month
+    @number_of_tweets_this_month = @tweets.tweets_this_month.count
   end
 
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,15 @@
 class Tweet < ApplicationRecord
   belongs_to :classification
   has_many :tweet_donation_joins
+
+  def current_month
+    month = Date.today.month
+    month
+  end
+
+  def tweets_this_month
+    this_months_tweets = Tweet.where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month)
+    return this_months_tweets
+  end
+
 end
